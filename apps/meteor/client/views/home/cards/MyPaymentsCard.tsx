c5e5dd068cde661818e72c642587f44821207649
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2023-09-25 16:37:02 +0530

    My Subscription Page UI Changes

--- apps/meteor/client/views/home/cards/MyPaymentsCard.tsx
+++ apps/meteor/client/views/home/cards/MyPaymentsCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Margins } from '@rocket.chat/fuselage';
+import { Box, Button, Margins, Icon } from '@rocket.chat/fuselage';
 import type { ReactElement } from 'react';
 import React from 'react';
 import { useSetModal } from '@rocket.chat/ui-contexts';
@@ -22,25 +22,51 @@ const MyPaymentsCard = (): ReactElement => {
 			<Margins block='x90'>
 				<Box is='div' m='auto' height='75%' width='75%' z={99} bg='light' color='default' >
 					<Box>
-						<Box is='header'
-						minHeight='x64'
-						p={16} 
-						mb={16}
-						textAlign='center'
-						fontSize='hero'
-						data-qa-id='my-subscription-header'>
-							My Subscription
+						<Box bg='#5627d8'>
+							<Box is='header'
+								minHeight='x64'
+								p={16}
+								mb={10}
+								textAlign='center'
+								fontSize='hero'
+								data-qa-id='my-subscription-header'
+							>
+								STANDARD
+							</Box>
+
+							<Box is='header'
+								minHeight='x64'
+								p={16}
+								mb={16}
+								textAlign='center'
+								fontSize='h3'
+								data-qa-id='my-subscription-header'
+							>
+								Quisque Vestibulum
+							</Box>
 						</Box>
-						<Box is='div' p={32}>
-							<ul>
+						<Box is='div' display='flex' justifyContent='center' alignItems='center' p={32} >
+							<Box fontSize='x64' color='green'>19$</Box><Box alignSelf='end' fontSize='x32' >Per Month</Box>
+						</Box>
+
+						<Box is='div'>
+							{/* <ul>
 								{resultData?.data?.resInfo.map((user: any) => {
 									return (
 										<li><strong>Name:</strong> {user?.name} <strong>UsernName:</strong>  {user?.username}</li>
 									)
 								})}
-							</ul>
+							</ul> */}
+							<Box display='flex' flexDirection='column' m='auto' width='50%' pis={50}>
+								<Box display='flex' alignItems='center' ><Icon name='success-circle' size='x40' />
+									<Box fontSize='h3' pis={10}>2X Speed</Box></Box>
+								<Box display='flex' alignItems='center' ><Icon name='success-circle' size='x40' />
+									<Box fontSize='h3' pis={10}>High Performance</Box></Box>
+								<Box display='flex' alignItems='center' ><Icon name='success-circle' size='x40' />
+									<Box fontSize='h3' pis={10}>Near-instant Provisioning</Box></Box>
+							</Box>
 						</Box>
-						<Box display='flex' justifyContent='flex-end' p={16} pbs={64}>
+						<Box display='flex' justifyContent='center' p={16} pbs={20}>
 							<Button m='2px' danger onClick={openCancelPaymentModal}>Cancel Subscription</Button>
 						</Box>
 					</Box>
